ec7ba2c34d1c66039cbb3fde683244939ba0ecad
katiRG <cnangini@gmail.com>
2018-11-21 11:19:20 -0500

    stacked area chart for fuel sales

---
+++ src/demo.js
@@ -0,0 +1,66 @@
+/* globals areaChart */
+var chart = d3.select(".data")
+    .append("svg")
+      .attr("id", "demo"),
+  id = "year",
+  settings = {
+      alt: i18next.t("alt", {ns: "area"}),
+      datatable: {
+        title: i18next.t("datatableTitle", {ns: "area"})
+      },
+      filterData: function(data) {
+        return data.fuelsales;
+      },
+      x: {
+
+        getValue: function(d) {
+          return new Date(d[id] + "-01");
+        },
+        getText: function(d) {
+          return d[id];
+        },
+        ticks: 7
+      },
+
+      y: {
+        label: i18next.t("y_label", {ns: "area"}),
+        getValue: function(d, key) {
+          if (typeof d[key] === 'string' || d[key] instanceof String) {
+            return 0;
+          }
+          else return d[key] * 1.0/ 1000;
+        }
+      },
+
+      z: {
+        label: i18next.t("z_label", {ns: "area"}),
+        getId: function(d) {
+          return d.key;
+        },
+        getKeys: function(object) {
+          var sett = this,
+          keys = Object.keys(object[0]);
+          keys.splice(keys.indexOf(id),1);
+          if (keys.indexOf(sett.y.totalProperty) !== -1) {
+            keys.splice(keys.indexOf(sett.y.totalProperty),1);
+          }
+          return keys;
+        },
+        getClass: function(d) {
+          return this.z.getId.apply(this, arguments);
+        },
+        getText: function(d) {
+          return i18next.t(d.key, {ns: "area"});
+        }
+      },
+      width: 900
+    };
+
+i18n.load(["src/i18n"], function() {
+  d3.queue()
+    // .defer(d3.json, "data/worldpop.json")
+    .defer(d3.json, "data/BC_FuelSales.json")
+    .await(function(error, data) {
+      areaChart(chart, settings, data);
+    });
+});
