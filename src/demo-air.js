36aa314197b8b2272651030b8ce83d31ebe61a78
katiRG <cnangini@gmail.com>
2018-11-24 17:21:20 -0500

    WIP: configure for air files

---
+++ src/demo-air.js
@@ -0,0 +1,133 @@
+data = {};
+selected = "CANADA";
+
+/* globals areaChart */
+var chart = d3.select(".data")
+    .append("svg")
+      .attr("id", "demo"),
+  id = "year",
+  settings = {
+      alt: i18next.t("alt", {ns: "area"}),
+      datatable: {
+        title: i18next.t("datatableTitle", {ns: "area"})
+      },
+      filterData: function(data) {
+        return data.numMov;
+      },
+      x: {
+
+        getValue: function(d) {
+          return new Date(d[id] + "-01");
+        },
+        getText: function(d) {
+          return d[id];
+        },
+        ticks: 7
+      },
+
+      y: {
+        label: i18next.t("y_label", {ns: "area"}),
+        getValue: function(d, key) {
+          if (typeof d[key] === 'string' || d[key] instanceof String) {
+            return 0;
+          }
+          else return d[key] * 1.0/ 1000;
+        },
+        getText: function(d, key) {
+          if (typeof d[key] === 'string' || d[key] instanceof String) {
+            return d[key];
+          }
+          else return d[key] * 1.0/ 1000;
+        }
+      },
+
+      z: {
+        label: i18next.t("z_label", {ns: "area"}),
+        getId: function(d) {
+          return d.key;
+        },
+        getKeys: function(object) {
+          console.log("object: ", object)
+          var sett = this,
+          keys = Object.keys(object[0]);
+          keys.splice(keys.indexOf(id),1);
+          if (keys.indexOf(sett.y.totalProperty) !== -1) {
+            keys.splice(keys.indexOf(sett.y.totalProperty),1);
+          }
+          return keys;
+        },
+        getClass: function(d) {
+          return this.z.getId.apply(this, arguments);
+        },
+        getText: function(d) {
+          return i18next.t(d.key, {ns: "area"});
+        }
+      },
+      width: 900
+    };
+
+  uiHandler = function(event) {
+    if (event.target.id === "groups"){
+      selected = document.getElementById("groups").value;
+      var labelsToClear = document.getElementsByClassName("area-label");
+      var i;
+      for (i = 0; i < labelsToClear.length; i++) {
+          labelsToClear[i].innerHTML='';
+      }
+      if (!data[selected]) {
+        console.log("selected: ", "data/" + selected + "_numMovements.json")
+        d3.json("data/" + selected + "_numMovements.json", function(err, filedata) {
+          data[selected] = filedata;
+          showData();
+         });
+     } else {
+       showData();
+     }
+    }
+  }
+
+  function showData() {
+    d3.select("#demo").select("g .data").text();
+    areaChart(chart, settings, data[selected]);
+  }
+
+  //
+//   data = {};
+// selected = "ca";
+
+// onInput = function() {
+//  selected = "nb"
+
+//  if (!data[selected]) {
+//    d3.json(selected + ".json", function(err, data) {
+//      showData();
+//    });
+//  } else {
+//    showData();
+//  }
+// }
+
+// showData() {
+//  areaChart(svg, settings, data[selected]);
+// }
+
+// d3.queue()
+//  .defer(d3.json, "ca.json")
+//  .await(funnction(err, caData) {
+//    data.ca = caData;
+//    showData();
+//  });
+ //
+
+i18n.load(["src/i18n"], function() {
+  d3.queue()
+    // .defer(d3.json, "data/worldpop.json")
+    .defer(d3.json, "data/BC_FuelSales.json")
+    .await(function(error, data) {
+      areaChart(chart, settings, data);
+    });
+});
+
+$(document).on("input change", function(event) {
+  uiHandler(event);
+});
