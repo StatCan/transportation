4e3826a8bda0545de9e538195b9b0ac0b1090d9c
katiRG <cnangini@gmail.com>
2018-11-25 09:56:07 -0500

    WIP contd: example data

--- src/demo-air.js
+++ src/demo-air.js
@@ -119,7 +119,101 @@ var chart = d3.select(".data")
     d3.select(".rank h4").text("Airport rank for " + i18next.t(selected, {ns: "airports"}));
 
 
+    //https://www.d3-graph-gallery.com/graph/correlogram_basic.html
+    // Graph dimension
+    var margin = {top: 20, right: 20, bottom: 20, left: 20},
+        width = 430 - margin.left - margin.right,
+        height = 430 - margin.top - margin.bottom
+
+    // Create the svg area
+    var svg = d3.select("#my_dataviz")
+      .append("svg")
+        .attr("width", width + margin.left + margin.right)
+        .attr("height", height + margin.top + margin.bottom)
+      .append("g")
+        .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+
+    var corrdata = [];
+    d3.csv("data/data_correlogram.csv", function(error, rows) {
+
+      // Going from wide to long format      
+      rows.forEach(function(d) {
+        var x = d[""];
+        delete d[""];
+        for (prop in d) {
+          var y = prop,
+            value = d[prop];
+          corrdata.push({
+            x: x,
+            y: y,
+            value: +value
+          });
+        }
+      });
+
+    
+    console.log("corr data: ", corrdata)
+
+    // List of all variables and number of them
+    var domain = d3.set(corrdata.map(function(d) { return d.x })).values()
+    var num = Math.sqrt(corrdata.length)
+
+    // Create a color scale
+    var color = d3.scaleLinear()
+      .domain([-1, 0, 1])
+      .range(["#B22222", "#fff", "#000080"]);
+
+    // Create a size scale for bubbles on top right. Watch out: must be a rootscale!
+    var size = d3.scaleSqrt()
+      .domain([0, 1])
+      .range([0, 9]);
+
+    // X scale
+    var x = d3.scalePoint()
+      .range([0, width])
+      .domain(domain)
+
+    // Y scale
+    var y = d3.scalePoint()
+      .range([0, height])
+      .domain(domain)
+
+    // Create one 'g' element for each cell of the correlogram
+    var cor = svg.attr("class", "rankplot")
+        .selectAll(".cor")
+      .data(corrdata)
+      .enter()
+      .append("g")
+        .attr("class", "cor")
+        .attr("transform", function(d) {
+          return "translate(" + x(d.x) + "," + y(d.y) + ")";
+          });
+    // Up right part: add circles
+    cor
+      .filter(function(d){
+        var ypos = domain.indexOf(d.y);
+        var xpos = domain.indexOf(d.x);
+        return true;
+        // if (xpos <= ypos) { return false } else { return true}
+      })
+      .append("circle")
+        .attr("r", function(d){ return size(Math.abs(d.value)) })
+        .style("fill", function(d){
+          if (d.x === d.y) {
+            return "#000";
+          } else {
+            return color(d.value);
+          }
+        })
+        .style("opacity", 0.8)
+
+    }) //end d3.csv
+  
+
   }
+
+
+
 
 i18n.load(["src/i18n"], function() {
   d3.queue()
