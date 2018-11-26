b98c6cee57670fc1df9df4091ae9ec905bb69b66
katiRG <cnangini@gmail.com>
2018-11-26 09:55:51 -0500

    radar plot for random data for rail dataset

--- src/demo-rail.js
+++ src/demo-rail.js
@@ -88,6 +88,219 @@ var chart = d3.select(".data")
     areaChart(chart, settings, data[selected]);
   }
 
+  function showRadar() {
+    //Adapted from http://bl.ocks.org/jeffthink/1630683
+    var series, 
+      hours,
+      minVal,
+      maxVal,
+      w = 400,
+      h = 400,
+      vizPadding = {
+          top: 10,
+          right: 0,
+          bottom: 15,
+          left: 100
+      },
+      radius,
+      radiusLength,
+      ruleColor = "#CCC";
+
+      loadData();
+      buildBase();
+      setScales();
+      addAxes();
+      draw();
+
+      function loadData() {
+        var randomFromTo = function randomFromTo(from, to){
+           return Math.floor(Math.random() * (to - from + 1) + from);
+        };
+
+        series = [
+          [],
+          []
+        ];
+
+        hours = [];
+
+        for (i = 0; i < 24; i += 1) {
+            series[0][i] = randomFromTo(0,20);
+            series[1][i] = randomFromTo(5,15);
+            hours[i] = i; //in case we want to do different formatting
+        }
+
+        mergedArr = series[0].concat(series[1]);
+
+        minVal = d3.min(mergedArr);
+        maxVal = d3.max(mergedArr);
+        //give 25% of range as buffer to top
+        maxVal = maxVal + ((maxVal - minVal) * 0.25);
+        minVal = 0;
+
+        //to complete the radial lines
+        for (i = 0; i < series.length; i += 1) {
+            series[i].push(series[i][0]);
+        }      
+    }
+
+    function buildBase() {
+        var viz = d3.select("#commgrid")
+            .append('svg:svg')
+            .attr('width', w)
+            .attr('height', h)
+            .attr('class', 'vizSvg');
+
+        viz.append("svg:rect")
+            .attr('id', 'axis-separator')
+            .attr('x', 0)
+            .attr('y', 0)
+            .attr('height', 0)
+            .attr('width', 0)
+            .attr('height', 0);
+        
+        vizBody = viz.append("svg:g")
+            .attr('id', 'body');
+    }
+
+    function setScales () {
+      var heightCircleConstraint,
+          widthCircleConstraint,
+          circleConstraint,
+          centerXPos,
+          centerYPos;
+
+      //need a circle so find constraining dimension
+      heightCircleConstraint = h - vizPadding.top - vizPadding.bottom;
+      widthCircleConstraint = w - vizPadding.left - vizPadding.right;
+      circleConstraint = d3.min([
+          heightCircleConstraint, widthCircleConstraint]);
+
+      radius = d3.scaleLinear().domain([minVal, maxVal])
+          .range([0, (circleConstraint / 2)]);
+      radiusLength = radius(maxVal);
+
+      //attach everything to the group that is centered around middle
+      centerXPos = widthCircleConstraint / 2 + vizPadding.left;
+      centerYPos = heightCircleConstraint / 2 + vizPadding.top;
+
+      vizBody.attr("transform",
+          "translate(" + centerXPos + ", " + centerYPos + ")");
+    }
+
+    function addAxes() {
+      var radialTicks = radius.ticks(5),
+          i,
+          circleAxes,
+          lineAxes;
+
+      vizBody.selectAll('.circle-ticks').remove();
+      vizBody.selectAll('.line-ticks').remove();
+
+      circleAxes = vizBody.selectAll('.circle-ticks')
+          .data(radialTicks)
+          .enter().append('svg:g')
+          .attr("class", "circle-ticks");
+
+      circleAxes.append("svg:circle")
+          .attr("r", function (d, i) {
+              return radius(d);
+          })
+          .attr("class", "circle")
+          .style("stroke", ruleColor)
+          .style("fill", "none");
+
+      circleAxes.append("svg:text")
+          .attr("text-anchor", "middle")
+          .attr("dy", function (d) {
+              return -1 * radius(d);
+          })
+          .text(String);
+
+      lineAxes = vizBody.selectAll('.line-ticks')
+          .data(hours)
+          .enter().append('svg:g')
+          .attr("transform", function (d, i) {
+              return "rotate(" + ((i / hours.length * 360) - 90) +
+                  ")translate(" + radius(maxVal) + ")";
+          })
+          .attr("class", "line-ticks");
+
+      lineAxes.append('svg:line')
+          .attr("x2", -1 * radius(maxVal))
+          .style("stroke", ruleColor)
+          .style("fill", "none");
+
+      lineAxes.append('svg:text')
+          .text(String)
+          .attr("text-anchor", "middle")
+          .attr("transform", function (d, i) {
+              return (i / hours.length * 360) < 180 ? null : "rotate(180)";
+          });
+    }
+
+    function draw  () {
+      var groups,
+          lines,
+          linesToUpdate;
+
+      highlightedDotSize = 4;
+
+      groups = vizBody.selectAll('.series')
+          .data(series);
+      groups.enter().append("svg:g")
+          .attr('class', 'series')
+          .style('fill', function (d, i) {
+              if(i === 0){
+                return "green";
+              } else {
+                return "blue";
+              }
+          })
+          .style('stroke', function (d, i) {
+              if(i === 0){
+                return "green";
+              } else {
+                return "blue";
+              }
+          });
+      groups.exit().remove();
+
+      // lines = groups.append('svg:path')
+      // lines = groups.enter().append('svg:path')
+      lines = d3.selectAll(".series").append("svg:path")
+          .attr("class", "line")
+          // .attr("d", d3.svg.line.radial()
+          .attr("d", d3.radialLine()
+              .radius(function (d) {
+                  return 0;
+              })
+              .angle(function (d, i) {
+                  if (i === 24) {
+                      i = 0;
+                  } //close the line
+                  return (i / 24) * 2 * Math.PI;
+              }))
+          .style("stroke-width", 3)
+          .style("fill", "none");
+
+      // lines.attr("d", d3.svg.line.radial()
+      lines.attr("d", d3.radialLine()
+          .radius(function (d) {
+              return radius(d);
+          })
+          .angle(function (d, i) {
+              if (i === 24) {
+                  i = 0;
+              } //close the line
+              return (i / 24) * 2 * Math.PI;
+          }));
+    }
+
+
+
+  }
+
   function showComm() {
      //change area chart title to match selected province
     d3.select(".commval h4").text("2016 tonnages for all commodities. Origin " + i18next.t("ATR", {ns: "regions"})
@@ -257,7 +470,8 @@ i18n.load(["src/i18n"], function() {
     .defer(d3.json, "data/rail_meat_origATR_ON_BC_destQC.json")
     .await(function(error, data) {
       areaChart(chart, settings, data);
-      showComm();
+      //showComm();
+      showRadar();
     });
 });
 
