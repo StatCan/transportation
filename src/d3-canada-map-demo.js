8371ad030f10014cc8774062a57b27814da66592
katiRG <cnangini@gmail.com>
2018-11-26 10:58:46 -0500

    airport circle above map

--- src/d3-canada-map-demo.js
+++ src/d3-canada-map-demo.js
@@ -1,21 +1,33 @@
 var map = d3.select(".dashboard .map")
 	.append("svg"),
+	pathGenerator = 
 	heading = d3.select(".dashboard h4"),
 	canada = window.getCanadaMap(map, {})
 		.on("loaded", function() {
-			window.console.log("loaded");
-		})
+			mapObj = this;
+			d3.json("geojson/testairport.geojson", function(error, airports) {
+			      if (error) throw error;
+			      console.log("airports: ", airports)
 
-// setInterval(function() {
-// 	var provinces = Object.keys(canada.provinces),
-// 		provincesLength = provinces.length,
-// 		show = Math.floor(Math.random() * (provincesLength + 1)),
-// 		province;
+			      var airportGroup = map.append("g");
+			      var path = d3.geoPath().projection(mapObj.settings.projection);
 
-// 	if (show < provincesLength){
-// 		province = provinces[show];
-// 		canada.provinces[province].zoom();
-// 	} else {
-// 		canada.zoom();
-// 	}
-// }, 2000);
+			      airportPoints = airportGroup.selectAll("path")
+			          .data(airports.features)
+			          .enter().append('path')
+			          .attr('d', path)
+			          .attr("id", function (d, i) {
+			            return "airport" + d.id;
+			          })
+			          .attr("class", "airport")
+			          .attr("r", 10)
+			          .style("fill", "red")
+			           .on("mouseover", function (d) {
+				            //change area chart title to match selected province
+    						d3.select(".dashboard h4").text(i18next.t("ON" + " and contribution from airport YYZ", {ns: "provinces"}));
+
+    						showAirport();
+				        });
+			        });
+
+		});
