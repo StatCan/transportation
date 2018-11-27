7b21835e1916b2213f7ae18900e2710f03fb10dd
katiRG <cnangini@gmail.com>
2018-11-26 20:21:00 -0500

    put map circles out of d3-canada-map-demo.js and into demo-air.js for AIR only

--- src/demo-air.js
+++ src/demo-air.js
@@ -1,6 +1,41 @@
 data = {};
 selected = "CANADA";
 
+/* canada map */
+var map = d3.select(".dashboard .map")
+  .append("svg"),
+  pathGenerator = 
+  heading = d3.select(".dashboard h4"),
+  canada = window.getCanadaMap(map, {})
+    .on("loaded", function() {
+      mapObj = this;
+      d3.json("geojson/testairport.geojson", function(error, airports) {
+            if (error) throw error;
+            console.log("airports: ", airports)
+
+            var airportGroup = map.append("g");
+            var path = d3.geoPath().projection(mapObj.settings.projection);
+
+            airportPoints = airportGroup.selectAll("path")
+                .data(airports.features)
+                .enter().append('path')
+                .attr('d', path)
+                .attr("id", function (d, i) {
+                  return "airport" + d.id;
+                })
+                .attr("class", "airport")
+                .attr("r", 10)
+                .style("fill", "red")
+                 .on("mouseover", function (d) {
+                    //change area chart title to match selected province
+                d3.select(".dashboard h4").text(i18next.t("ON" + " and contribution from airport YYZ", {ns: "provinces"}));
+
+                showAirport();
+                });
+              });
+
+    });
+
 /* globals areaChart */
 var chart = d3.select(".data")
     .append("svg")
