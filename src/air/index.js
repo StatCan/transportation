6637331bcbb3e768970ad0be8241cd3d7913368b
katiRG <cnangini@gmail.com>
2018-11-30 16:11:02 -0500

    WIP: refactored

--- src/air/index.js
+++ src/air/index.js
@@ -1,12 +1,17 @@
-data = {};
-selected = "CANADA";
+import settings from "./settings.js";
+
+let map = d3.select(".dashboard .map")
+  .append("svg");
+let chart = d3.select(".data")
+    .append("svg")
+      .attr("id", "demo");
+
+let data = {};
+let selected = "CANADA";
 
 /* canada map */
-var map = d3.select(".dashboard .map")
-  .append("svg"),
-  heading = d3.select(".dashboard h4"),
-  canada = window.getCanadaMap(map, {})
-    .on("loaded", function() {
+let heading = d3.select(".dashboard h4"),
+  canada = window.getCanadaMap(map).on("loaded", function() {
       mapObj = this;
       d3.json("geojson/testairport.geojson", function(error, airports) {
             if (error) throw error;
@@ -36,106 +41,32 @@ var map = d3.select(".dashboard .map")
 
     });
 
-/* globals areaChart */
-var chart = d3.select(".data")
-    .append("svg")
-      .attr("id", "demo"),
-  id = "year",
-  settings = {
-      alt: i18next.t("alt", {ns: "area"}),
-      datatable: {
-        title: i18next.t("datatableTitle", {ns: "area"})
-      },
-      filterData: function(data) {
-        return data.numMov;
-      },
-      x: {
-
-        getValue: function(d) {
-          return new Date(d[id] + "-01");
-        },
-        getText: function(d) {
-          return d[id];
-        },
-        ticks: 7
-      },
-
-      y: {
-        label: i18next.t("y_label", {ns: "area"}),
-        getValue: function(d, key) {
-          if (typeof d[key] === 'string' || d[key] instanceof String) {
-            return 0;
-          }
-          else return d[key] * 1.0/ 1000;
-        },
-        getText: function(d, key) {
-          if (typeof d[key] === 'string' || d[key] instanceof String) {
-            return d[key];
-          }
-          else return d[key] * 1.0/ 1000;
-        }
-      },
-
-      z: {
-        label: i18next.t("z_label", {ns: "area"}),
-        getId: function(d) {
-          return d.key;
-        },
-        getKeys: function(object) {
-          var sett = this,
-          keys = Object.keys(object[0]);
-          keys.splice(keys.indexOf(id),1);
-          if (keys.indexOf(sett.y.totalProperty) !== -1) {
-            keys.splice(keys.indexOf(sett.y.totalProperty),1);
-          }
-          return keys;
-        },
-        getClass: function(d) {
-          return this.z.getId.apply(this, arguments);
-        },
-        getText: function(d) {
-          return i18next.t(d.key, {ns: "area"});
-        }
-      },
-      width: 900
-    };
-
-  uiHandler = function(event) {
+   function uiHandler(event) {
     if (event.target.id === "groups"){
       selected = document.getElementById("groups").value;
-      var labelsToClear = document.getElementsByClassName("area-label");
-      var i;
-      for (i = 0; i < labelsToClear.length; i++) {
-          labelsToClear[i].innerHTML='';
-      }
-      if (!data[selected]) {
-        d3.json("data/" + selected + "_numMovements.json", function(err, filedata) {
-          data[selected] = filedata;
-          showData();
-         });
-     } else {
-       showData();
-     }
+      showData();
     }
   }
 
   function showData() {
+    let showChart = () => {
+      areaChart(chart, settings, data[selected]);
+    }
     //change area chart title to match selected province
     d3.select(".dashboard h4").text(i18next.t(selected, {ns: "provinces"}));
 
-    areaChart(chart, settings, data[selected]);
+    if (!data[selected]) {
+      return d3.json(`data/air/${selected}_numMovements.json`, (ptData) => {
+        data[selected] = ptData;
+        showChart();
+      });
+    }
+    showChart();
   }
 
   function showAirport() {
-    //clear area labels
-    var labelsToClear = document.getElementsByClassName("area-label");
-    var i;
-    for (i = 0; i < labelsToClear.length; i++) {
-        labelsToClear[i].innerHTML='';
-    }
-
     //Load airport data containing remaining provincial totals
-    d3.json("data/combo_ON_ONYOW_numMovements.json", function(err, filedata) {
+    d3.json("data/air/combo_ON_ONYOW_numMovements.json", function(err, filedata) {
        selected = "ON_YOW";
        data[selected] = filedata;
 
@@ -288,18 +219,5 @@ var chart = d3.select(".data")
 
   }
 
-
-
-
-i18n.load(["src/i18n"], function() {
-  d3.queue()
-    // .defer(d3.json, "data/worldpop.json")
-    .defer(d3.json, "data/CANADA_numMovements.json")
-    .await(function(error, data) {
-      areaChart(chart, settings, data);
-    });
-});
-
-$(document).on("input change", function(event) {
-  uiHandler(event);
-});
+i18n.load(["src/i18n"], showData);
+$(document).on("change", uiHandler);
---
+++ src/air/settings.js
@@ -0,0 +1,58 @@
+export default {
+  alt: i18next.t("alt", {ns: "area"}),
+  datatable: {
+    title: i18next.t("datatableTitle", {ns: "area"})
+  },
+  filterData: function(data) {
+    return data.numMov;
+  },
+  x: {
+
+    getValue: function(d) {
+      return new Date(d.year + "-01");
+    },
+    getText: function(d) {
+      return d.year;
+    },
+    ticks: 7
+  },
+
+  y: {
+    label: i18next.t("y_label", {ns: "area"}),
+    getValue: function(d, key) {
+      if (typeof d[key] === 'string' || d[key] instanceof String) {
+        return 0;
+      }
+      else return d[key] * 1.0/ 1000;
+    },
+    getText: function(d, key) {
+      if (typeof d[key] === 'string' || d[key] instanceof String) {
+        return d[key];
+      }
+      else return d[key] * 1.0/ 1000;
+    }
+  },
+
+  z: {
+    label: i18next.t("z_label", {ns: "area"}),
+    getId: function(d) {
+      return d.key;
+    },
+    getKeys: function(object) {
+      var sett = this,
+      keys = Object.keys(object[0]);
+      keys.splice(keys.indexOf("year"),1);
+      if (keys.indexOf(sett.y.totalProperty) !== -1) {
+        keys.splice(keys.indexOf(sett.y.totalProperty),1);
+      }
+      return keys;
+    },
+    getClass: function(d) {
+      return this.z.getId.apply(this, arguments);
+    },
+    getText: function(d) {
+      return i18next.t(d.key, {ns: "area"});
+    }
+  },
+  width: 900
+}
