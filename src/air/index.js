d1abf5410d4a6b1ddacfe6030e832b96ea7d29d4
katiRG <cnangini@gmail.com>
2018-11-30 16:24:42 -0500

    defined mapObj for airport circle json

--- src/air/index.js
+++ src/air/index.js
@@ -11,97 +11,96 @@ let selected = "CANADA";
 
 /* canada map */
 let heading = d3.select(".dashboard h4"),
-  canada = window.getCanadaMap(map).on("loaded", function() {
-      mapObj = this;
-      d3.json("geojson/testairport.geojson", function(error, airports) {
-            if (error) throw error;
-            console.log("airports: ", airports)
+canada = window.getCanadaMap(map).on("loaded", function() {
+  console.log("here: ", this)
+  let mapObj = this;
+  d3.json("geojson/testairport.geojson", function(error, airports) {
+    console.log(airports)
+    if (error) throw error;
+    console.log("airports: ", airports)
 
-            var airportGroup = map.append("g");
-            var path = d3.geoPath().projection(mapObj.settings.projection)
-                        .pointRadius(2);
+    var airportGroup = map.append("g");
+    var path = d3.geoPath().projection(mapObj.settings.projection)
+                .pointRadius(2);
 
-            airportPoints = airportGroup.selectAll("path")
-                .data(airports.features)
-                .enter().append('path')
-                .attr('d', path)
-                .attr("id", function (d, i) {
-                  return "airport" + d.id;
-                })
-                .attr("class", "airport")
-                //.attr("r", 50)
-                .style("fill", "#7E0C33")
-                 .on("mouseover", function (d) {
-                    //change area chart title to match selected province
-                d3.select(".dashboard h4").text(i18next.t("ON" + " and contribution from airport YYZ", {ns: "provinces"}));
+    airportPoints = airportGroup.selectAll("path")
+        .data(airports.features)
+        .enter().append('path')
+        .attr('d', path)
+        .attr("id", function (d, i) {
+          return "airport" + d.id;
+        })
+        .attr("class", "airport")
+        //.attr("r", 50)
+        .style("fill", "#7E0C33")
+         .on("mouseover", function (d) {
+            //change area chart title to match selected province
+        d3.select(".dashboard h4").text(i18next.t("ON" + " and contribution from airport YYZ", {ns: "provinces"}));
 
-                showAirport();
-                });
-              });
+        showAirport();
+        });
+  });
+});
 
+function uiHandler(event) {
+  if (event.target.id === "groups"){
+    selected = document.getElementById("groups").value;
+    showData();
+  }
+}
+
+function showData() {
+  let showChart = () => {
+    areaChart(chart, settings, data[selected]);
+  }
+  //change area chart title to match selected province
+  d3.select(".dashboard h4").text(i18next.t(selected, {ns: "provinces"}));
+
+  if (!data[selected]) {
+    return d3.json(`data/air/${selected}_numMovements.json`, (ptData) => {
+      data[selected] = ptData;
+      showChart();
     });
-
-   function uiHandler(event) {
-    if (event.target.id === "groups"){
-      selected = document.getElementById("groups").value;
-      showData();
-    }
   }
+  showChart();
+}
 
-  function showData() {
-    let showChart = () => {
-      areaChart(chart, settings, data[selected]);
-    }
-    //change area chart title to match selected province
-    d3.select(".dashboard h4").text(i18next.t(selected, {ns: "provinces"}));
+function showAirport() {
+  //Load airport data containing remaining provincial totals
+  d3.json("data/air/combo_ON_ONYOW_numMovements.json", function(err, filedata) {
+     selected = "ON_YOW";
+     data[selected] = filedata;
 
-    if (!data[selected]) {
-      return d3.json(`data/air/${selected}_numMovements.json`, (ptData) => {
-        data[selected] = ptData;
-        showChart();
-      });
-    }
-    showChart();
-  }
+    showData();
+    
+  });
 
-  function showAirport() {
-    //Load airport data containing remaining provincial totals
-    d3.json("data/air/combo_ON_ONYOW_numMovements.json", function(err, filedata) {
-       selected = "ON_YOW";
-       data[selected] = filedata;
+  //show airport rank
+  selectedAirport = "YOW";
+  showRank(selectedAirport);
+}
 
-      showData();
-      
-    });
+function showRank(selected) {
+  //change area chart title to match selected province
+  d3.select(".rank h4").text("Airport rank for " + i18next.t(selected, {ns: "airports"}));
 
-    //show airport rank
-    selectedAirport = "YOW";
-    showRank(selectedAirport);
-  }
+  //Adapted from: https://www.d3-graph-gallery.com/graph/correlogram_basic.html
+  // Graph dimension
+  var margin = {top: 20, right: 20, bottom: 20, left: 90},
+      width = 1100 - margin.left - margin.right,
+      height = 430 - margin.top - margin.bottom;
 
-  function showRank(selected) {
-    //change area chart title to match selected province
-    d3.select(".rank h4").text("Airport rank for " + i18next.t(selected, {ns: "airports"}));
+  // Create the svg area
+  var svg = d3.select("#my_dataviz")
+    .append("svg")
+      .attr("width", width + margin.left + margin.right)
+      .attr("height", height + margin.top + margin.bottom)
+    .append("g")
+      .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
 
-
-    //Adapted from: https://www.d3-graph-gallery.com/graph/correlogram_basic.html
-    // Graph dimension
-    var margin = {top: 20, right: 20, bottom: 20, left: 90},
-        width = 1100 - margin.left - margin.right,
-        height = 430 - margin.top - margin.bottom;
-
-    // Create the svg area
-    var svg = d3.select("#my_dataviz")
-      .append("svg")
-        .attr("width", width + margin.left + margin.right)
-        .attr("height", height + margin.top + margin.bottom)
-      .append("g")
-        .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
-
-    var corrdata = [];
-    d3.csv("data/rankdata_YOW.csv", function(error, rows) {
-
-      rows.forEach(function(d) {
+  var corrdata = [];
+  d3.csv("data/rankdata_YOW.csv", function(error, rows) {
+    rows.forEach(function(d) {
       var x = d[""];
       delete d[""];
       for (prop in d) {
@@ -147,7 +146,7 @@ let heading = d3.select(".dashboard h4"),
 
     // Create one 'g' element for each cell of the correlogram
     var cor = svg.attr("class", "rankplot")
-        .selectAll(".cor")
+      .selectAll(".cor")
       .data(corrdata)
       .enter()
       .append("g")
@@ -160,64 +159,65 @@ let heading = d3.select(".dashboard h4"),
           // return "translate(" + x(d.x) + "," + y(d.y) + ")";
           // return "translate(" + x(d.x) + "," + y(d.value) + ")";
           return "translate(" + x(d.x) + "," + ycoord + ")";
-          });
+        });
 
     // add circles
     cor
       .append("circle")
-          .attr("class", function(d) {
-            return "rank_" + d.y;
-          })
-          .attr("r", function(d){
-            return size(Math.abs(d.value));
-          })
-          .style("fill", function(d){
-              // return color(d.value);
-            });
+        .attr("class", function(d) {
+          return "rank_" + d.y;
+        })
+        .attr("r", function(d){
+          return size(Math.abs(d.value));
+        })
+        .style("fill", function(d){
+          // return color(d.value);
+        });
+
     //label columns by year
     cor.append("text")
-        .attr("dx", function(d){
-          return -18;
-        })
-        .attr("dy", function(d){
-          return -30;
-        })
-        .attr("class", "rank_yr")
-        .text(function(d,i){
-          if (d.y === "total") return d.x;
-        });
+      .attr("dx", function(d){
+        return -18;
+      })
+      .attr("dy", function(d){
+        return -30;
+      })
+      .attr("class", "rank_yr")
+      .text(function(d,i){
+        if (d.y === "total") return d.x;
+      });
 
     //label rows by movt type
     cor.append("text")
-        .attr("dx", function(d){
-          return -85;
-        })
-        .attr("dy", function(d){
-          return 4;
-        })
-        .attr("class", "rank_type")
-        .text(function(d,i){
-          if (d.x === "1997") return i18next.t(d.y, {ns: "area"});
-        });
+      .attr("dx", function(d){
+        return -85;
+      })
+      .attr("dy", function(d){
+        return 4;
+      })
+      .attr("class", "rank_type")
+      .text(function(d,i){
+        if (d.x === "1997") return i18next.t(d.y, {ns: "area"});
+      });
 
     //label circle by value
     cor.append("text")
-        .attr("dx", function(d){
-          if (d.y === "local") return -9;
-          else return -5;
-        })
-        .attr("dy", function(d){
-          return 4;
-        })
-        .attr("class", "rank_value")
-        .text(function(d,i){
-          return d.value;
-        });
+      .attr("dx", function(d){
+        if (d.y === "local") return -9;
+        else return -5;
+      })
+      .attr("dy", function(d){
+        return 4;
+      })
+      .attr("class", "rank_value")
+      .text(function(d,i){
+        return d.value;
+      });
 
-    }) //end d3.csv
-  
+  }) //end d3.csv
 
-  }
+
+}
 
 i18n.load(["src/i18n"], showData);
 $(document).on("change", uiHandler);
