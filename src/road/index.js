ca588dc8e9761e303201bc043df38da66c20afd6
katiRG <cnangini@gmail.com>
2019-01-22 17:20:52 -0500

    first working versions for 3 modes

--- src/road/index.js
+++ src/road/index.js
@@ -1,53 +1,25 @@
 import settings from "./stackedAreaSettings.js";
 
 const data = {};
+const mapData = {};
 let selected = "CANADA";
+let selectedYear = 2017;
+const units = "$";
+const xaxisLabeldy = "2.5em";
 
 const map = d3.select(".dashboard .map")
     .append("svg");
 getCanadaMap(map).on("loaded", function() {
   d3.select(".dashboard .map").selectAll("path").style("stroke", "black");
 
-  const fakeTotDict = {
-    "BC": 4935834,
-    "AB": 6368800,
-    "SK": 1730149,
-    "MB": 1614445,
-    "ON": 16669930,
-    "QC": 8820509,
-    "NB": 1128764,
-    "NS": 1252000,
-    "PE": 216552,
-    "NL": 749758,
-    "NT": 42568,
-    "NU": 15507,
-    "YT": 72077
-  };
-
-  const totArr = [];
-  for (var key in fakeTotDict) {
-    totArr.push(fakeTotDict[key])
-  }
-
-  // https://d3js.org/colorbrewer.v1.js
-  const colourArray= ["#eff3ff", "#bdd7e7", "#6baed6", "#3182bd", "#08519c"];
-
-  totArr.sort(function(a, b) {
-    return a-b;
-  });
-
-  const dimExtent = d3.extent(totArr);
-
-  // colour map to take data value and map it to the colour of the level bin it belongs to
-  const colourMap = d3.scaleLinear()
-      .domain([dimExtent[0], dimExtent[1]])
-      .range(colourArray);
-
-  for (const key in fakeTotDict) {
-    if (fakeTotDict.hasOwnProperty(key)) {
-      d3.select(".dashboard .map")
-          .select("." + key).style("fill", colourMap(fakeTotDict[key]));
-    }
+  // Read map data
+  if (!mapData[selectedYear]) {
+    d3.json("data/road/" + selected + ".json", function(err, filedata) {
+      mapData[selectedYear] = filedata;
+      showChloropleth();
+    });
+  } else {
+    showChloropleth();
   }
 }); // end map
 
@@ -83,7 +55,7 @@ map.on("click", () => {
 
   // Display selected region in stacked area chart
   if (!data[selected]) {
-    d3.json("data/road/" + selected + "_FuelSales.json", function(err, filedata) {
+    d3.json("data/road/" + selected + ".json", function(err, filedata) {
       data[selected] = filedata;
       showData();
     });
@@ -97,13 +69,13 @@ map.on("click", () => {
 /* globals areaChart */
 const chart = d3.select(".data")
     .append("svg")
-    .attr("id", "demo");
+    .attr("id", "svgFuel");
 
 function uiHandler(event) {
   if (event.target.id === "groups") {
     selected = document.getElementById("groups").value;
     if (!data[selected]) {
-      d3.json("data/road/" + selected + "_FuelSales.json", function(err, filedata) {
+      d3.json("data/road/" + selected + ".json", function(err, filedata) {
         data[selected] = filedata;
         showData();
       });
@@ -111,21 +83,72 @@ function uiHandler(event) {
       showData();
     }
   }
+  if (event.target.id === "year") {
+    selectedYear = document.getElementById("year").value;
+    console.log("selectedYear: ", selectedYear);
+  }
+}
+
+function showChloropleth() {
+  const totalDict = {
+    "BC": 6931659,
+    "AB": 10274500,
+    "SK": 3239294,
+    "MB": 2484063,
+    "ON": 22195686,
+    "QC": 11862943,
+    "NB": 1612933,
+    "NS": 1668092,
+    "PE": 264135,
+    "NL": 1097819,
+    "NT": 180499,
+    "NU": 38501,
+    "YT": 126956
+  };
+
+  const totArr = [];
+  for (var key in totalDict) {
+    totArr.push(totalDict[key])
+  }
+
+  // https://d3js.org/colorbrewer.v1.js
+  const colourArray= ["#eff3ff", "#bdd7e7", "#6baed6", "#3182bd", "#08519c"];
+
+  totArr.sort(function(a, b) {
+    return a-b;
+  });
+
+  const dimExtent = d3.extent(totArr);
+
+  // colour map to take data value and map it to the colour of the level bin it belongs to
+  const colourMap = d3.scaleLinear()
+      .domain([dimExtent[0], dimExtent[1]])
+      .range(colourArray);
+
+  for (const key in totalDict) {
+    if (totalDict.hasOwnProperty(key)) {
+      d3.select(".dashboard .map")
+          .select("." + key).style("fill", colourMap(totalDict[key]));
+    }
+  }
+
+  // colour bar scale
+  mapColourScaleFn(colourArray, dimExtent, units);
 }
 
 function showData() {
-  // change area chart title to match selected province
-  d3.select(".dashboard h4").text(i18next.t(selected, {ns: "provinces"}));
   areaChart(chart, settings, data[selected]);
+  d3.select("#svgFuel").select(".x.axis").select("text").attr("dy", xaxisLabeldy);
 }
 
 i18n.load(["src/i18n"], () => {
   settings.x.label = i18next.t("x_label", {ns: "roadArea"}),
   settings.y.label = i18next.t("y_label", {ns: "roadArea"}),
   d3.queue()
-      .defer(d3.json, "data/road/CANADA_FuelSales.json")
+      .defer(d3.json, "data/road/CANADA.json")
       .await(function(error, data) {
         areaChart(chart, settings, data);
+        d3.select("#svgFuel").select(".x.axis").select("text").attr("dy", xaxisLabeldy);
       });
 });
 
--- src/road/stackedAreaSettings.js
+++ src/road/stackedAreaSettings.js
@@ -1,42 +1,60 @@
 export default {
-  alt: i18next.t("alt", {ns: "area"}),
+  alt: i18next.t("alt", {ns: "roadArea"}),
   margin: {
-    left: 80
+    top: 50,
+    left: 80,
+    bottom: 50
   },
+  //creates variable d
   filterData: function(data) {
-    return data.fuelsales;
+    return data;
   },
   x: {
     getValue: function(d) {
-      return new Date(d.year + "-01");
+      return new Date(d.date + "-01");
     },
     getText: function(d) {
-      return d.year;
+      return d.date;
     },
     ticks: 7
   },
   y: {
-    label: i18next.t("y_label", {ns: "area"}),
+    label: i18next.t("y_label", {ns: "roadArea"}),
     getValue: function(d, key) {
       if (typeof d[key] === "string" || d[key] instanceof String) {
         return 0;
       } else return d[key] * 1.0/ 1000;
     },
+    getTotal: function(d, index, data) {
+      var total; var keys; var sett = this;
+      if (!d[sett.y.totalProperty]) {
+        keys = sett.z.getKeys.call(sett, data);
+        total = 0;
+        //skipped date value here slice later
+        for(var k = 0; k < keys.length; k++) {
+          total += sett.y.getValue.call(sett, d, keys[k], data);
+        }
+        d[sett.y.totalProperty] = total;
+      }
+
+      return d[sett.y.totalProperty];
+    },
     getText: function(d, key) {
       if (typeof d[key] === "string" || d[key] instanceof String) {
         return d[key];
       } else return d[key] * 1.0/ 1000;
-    }
+    },
+    ticks: 5
   },
   z: {
-    label: i18next.t("z_label", {ns: "area"}),
+    label: i18next.t("z_label", {ns: "roadArea"}),
     getId: function(d) {
       return d.key;
     },
     getKeys: function(object) {
       const sett = this;
       const keys = Object.keys(object[0]);
-      keys.splice(keys.indexOf("year"), 1);
+      keys.splice(keys.indexOf("date"), 1);
       if (keys.indexOf(sett.y.totalProperty) !== -1) {
         keys.splice(keys.indexOf(sett.y.totalProperty), 1);
       }
@@ -46,7 +64,7 @@ export default {
       return this.z.getId.apply(this, arguments);
     },
     getText: function(d) {
-      return i18next.t(d.key, {ns: "area"});
+      return i18next.t(d.key, {ns: "roadArea"});
     }
   },
   datatable: false,
