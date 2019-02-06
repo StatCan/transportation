401f27f416c260bd849c31b77e976c60d747facf
katiRG <cnangini@gmail.com>
2019-02-06 10:37:26 -0500

    RAIL: cleaned and works for one input comm and region

--- src/rail/index.js
+++ src/rail/index.js
@@ -2,12 +2,12 @@ import settings from "./settings_lineChart.js";
 import settBubble from "./settings_bubbleTable.js";
 import createLegend from "./createLegend.js";
 
-const data = {};
-let selectedRegion = "ATR";
-let selectedComm = "meat";
+// const data = {};
+let selectedRegion = "ON";
+let selectedComm = "coal";
 
-// const regions = ["ATR", "QC", "ON", "MB", "SK", "AB", "BC", "USMEX"];
-const regions = ["ATR", "ON", "QC", "MB", "SK", "AB", "BC"];
+// const regions = ["AT", "QC", "ON", "MB", "SK", "AB", "BC", "US-MEX"];
+const regions = ["AT", "ON", "QC", "MB", "SK", "AB", "BC"];
 const remainingRegions = regions.filter((item) => item !== selectedRegion);
 
 // const formatNumber = d3.format(","); // d3.format(".2f");
@@ -61,30 +61,27 @@ function uiHandler(event) {
     selectedRegion= document.getElementById("region").value;
   }
 
-  if (!data[selectedRegion]) {
-    // Read in chosen region as ORIGIN
-    d3.json("data/rail/rail_" + selectedComm + "_orig" + selectedRegion+ "_all_dest.json", function(err, fileorig) {
-      data[selectedRegion] = fileorig;
-    });
-  } else {
-    showArea();
-  }
+  // TO DO
+  // if (!data[selectedRegion]) {
+  //   // Read in chosen region as ORIGIN
+  //   d3.json("data/rail/rail_" + selectedComm + "_orig" + selectedRegion+ "_all_dest.json", function(err, fileorig) {
+  //     data[selectedRegion] = fileorig;
+  //   });
+  // } else {
+  //   showArea();
+  // }
 }
 
 // ---------------------------------------------------------------------
-function showArea() {
-  lineChart(chartPair1, settings, data[selectedRegion]);
-}
+// function showArea() {
+//   lineChart(chartPair1, settings, data[selectedRegion]);
+// }
 function showComm(region) {
-  // const thisReg = i18next.t(region, {ns: "railRegions"});
-  // const thisText = `Commodities originating from ${thisReg}, total tonnage (millions) for all destinations`;
   const thisText = "Total tonnage from all origins to all destinations (x 1M) for 10 commodities";
   d3.select("#commTableTitle")
       .text(thisText);
 
   // Read commodities file for selected region
-  // d3.json("data/rail/commdata_" + selectedRegion + ".json", function(err, json) {
-  // d3.json("data/rail/commdata_QC.json", function(err, json) {
   d3.json("data/rail/commdata_allOrig_allDest.json", function(err, json) {
     sortComm(json);
     bubbleTable(commTable, settBubble, json);
@@ -101,44 +98,44 @@ i18n.load(["src/i18n"], function() {
   settings.y.label = i18next.t("y_label", {ns: "railArea"}),
   // settBubble.z.getText = i18next.t("y_label", {ns: "commodities"}),
 
-  d3.json("data/rail/" + selectedComm + "_" + selectedRegion + ".json", function(err, json1) {
-    data[selectedRegion] = json1;
-    const numYears = json1.length;
+  d3.json("data/rail/" + selectedRegion + "_" + selectedComm + ".json", function(err, json1) {
+    console.log("json1: ", json1);
+    const numYears = Object.keys(json1).length;
 
     for (let idx = 0; idx < remainingRegions.length; idx++) {
-      const thisReg = remainingRegions[idx];
-      d3.json("data/rail/" + selectedComm + "_" + thisReg + ".json", function(err, json2) {
-        data[thisReg] = json2;
-
+      const targetRegion = remainingRegions[idx];
+      d3.json("data/rail/" + targetRegion + "_" + selectedComm + ".json", function(err, json2) {
         // Construct data object pair to send to stacked area chart
         const arrPair = [];
         for (let idx = 0; idx < numYears; idx++) {
+          const thisYear = Object.keys(json1)[idx];
           arrPair.push({
-            year: data[thisReg][idx].year,
-            [selectedRegion + "to" + thisReg]: data[selectedRegion][idx][thisReg],
-            [thisReg + "to" + selectedRegion]: data[thisReg][idx][selectedRegion]
+            year: thisYear,
+            [selectedRegion + "to" + targetRegion]: json1[thisYear][targetRegion],
+            [targetRegion + "to" + selectedRegion]: json2[thisYear][selectedRegion]
           });
         }
+        // console.log("arrPair: ", arrPair);
 
         // send to lineChart
         if (idx == 0) {
           lineChart(chartPair1, settings, arrPair);
-          createLegend([selectedRegion, thisReg], "#legend1");
+          createLegend([selectedRegion, targetRegion], "#legend1");
         } else if (idx == 1) {
           lineChart(chartPair2, settings, arrPair);
-          createLegend([selectedRegion, thisReg], "#legend2");
+          createLegend([selectedRegion, targetRegion], "#legend2");
         } else if (idx == 2) {
           lineChart(chartPair3, settings, arrPair);
-          createLegend([selectedRegion, thisReg], "#legend3");
+          createLegend([selectedRegion, targetRegion], "#legend3");
         } else if (idx == 3) {
           lineChart(chartPair4, settings, arrPair);
-          createLegend([selectedRegion, thisReg], "#legend4");
+          createLegend([selectedRegion, targetRegion], "#legend4");
         } else if (idx == 4) {
           lineChart(chartPair5, settings, arrPair);
-          createLegend([selectedRegion, thisReg], "#legend5");
+          createLegend([selectedRegion, targetRegion], "#legend5");
         } else if (idx == 5) {
           lineChart(chartPair6, settings, arrPair);
-          createLegend([selectedRegion, thisReg], "#legend6");
+          createLegend([selectedRegion, targetRegion], "#legend6");
         }
       }); // inner d3.json
     } // for loop
