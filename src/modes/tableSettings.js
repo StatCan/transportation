c46d15cc0b2bc7f86e1c9f6d7d2c51830d861da2
katiRG <cnangini@gmail.com>
2019-02-09 14:40:48 -0500

    WIP done. Sankey table text formatted

--- src/modes/tableSettings.js
+++ src/modes/tableSettings.js
@@ -8,8 +8,6 @@ export default {
   },
   // creates variable d
   filterData: function(data) {
-    console.log("tableOnly filterData: ", data)
-    console.log("data.nodes: ", data.nodes)
     return data.nodes; // array of objects
   },
   x: {
@@ -24,33 +22,36 @@ export default {
   },
   y: {
     getValue: function(d, key) {
-      console.log("d: ", d)
-      // if (d.targetLinks[0]) {
-      //   console.log("d.targetLinks: ", d.targetLinks)
-      //   console.log("d.targetLinks: ", d.targetLinks[0])
-      //   console.log("d.targetLinks: ", d.targetLinks[0].source)
-      //   console.log("d.targetLinks: ", d.targetLinks[0].source.name)
-      // }
-      console.log("key: ", key)
-      console.log("d[key]: ", d[key])
-
       // d[key] can be string for Traveller Type, number for Count
       if (typeof d[key] === "string" || d[key] instanceof String) { // Traveller Type
-        if (d.targetLinks[0]) { // not first node
+        if (d.targetLinks[0]) { // empty only for first node
+          // Land cases, parent is two levels up
+          if (d.name === "USres_car" || d.name === "USres_bus" || 
+              d.name === "USres_train" || d.name === "USres_other") {
+            const parent = i18next.t("USres", {ns: "modes"});
+            const child = i18next.t(d[key], {ns: "modes"});
+            return parent + ", " + child;
+          } else if (d.name === "cdnFromUS_car" || d.name === "cdnFromUS_bus" || 
+              d.name === "cdnFromUS_train" || d.name === "cdnFromUS_other") {
+            const parent = i18next.t("cdnFromUS", {ns: "modes"});
+            const child = i18next.t(d[key], {ns: "modes"});
+            return parent + ", " + child;
+
+          } else {
+          // All other cases, parent is in targetLinks[0].source.name
           const parent = i18next.t(d.targetLinks[0].source.name, {ns: "modes"});
           const child = i18next.t(d[key], {ns: "modes"});
-          console.log("parent: ", parent);
-          console.log("child: ", child);
           return parent + ", " + child;
-        } else if (d.name === "cdnFromOther_land") {
-          const parent = i18next.t("cdnFromOther", {ns: "modes"});
-          const child = i18next.t(d[key], {ns: "modes"});
-          console.log("parent: ", parent);
-          console.log("child: ", child);
-          return parent + ", " + child;
+         }
+        } else { // targetLinks empty
+          if (d.name === "cdnFromOther_land") { // special case
+            const parent = i18next.t("cdnFromOther", {ns: "modes"});
+            const child = i18next.t(d[key], {ns: "modes"});
+            return parent + ", " + child;
+          }
         }
-        return i18next.t(d[key], {ns: "modes"}); // first node has no parent
-      } else return d[key];
+        return i18next.t(d[key], {ns: "modes"}); // targetLinks empty (first node has no parent)
+      } else return d[key]; // Number, not a string, do not pass through i18next
     }
     // getTotal: function(d, index, data) {
     //   let total;
@@ -80,12 +81,9 @@ export default {
     //   return d.key;
     // },
     getKeys: function(object) {
-      console.log("z getKeys object: ", object)
       const sett = this;
       // const keys = Object.keys(object[0]);
       const keys = ["name", "value"];
-      console.log("z keys: ", keys)
-      // console.log("z keys slice: ", Object.keys(object[0]).slice(1)); // exclude first key
 
       if (keys.indexOf(sett.y.totalProperty) !== -1) {
         keys.splice(keys.indexOf(sett.y.totalProperty), 1);
