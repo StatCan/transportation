46b5e2a3535673caf1eacaf7f7870fde6ddbdd24
katiRG <cnangini@gmail.com>
2019-02-13 23:16:58 -0500

    WIP sankey: attempt to layout node y-coords

--- src/modes/index.js
+++ src/modes/index.js
@@ -1,7 +1,7 @@
 import makeSankey from "./makeSankey.js";
 import tableSettings from "./tableSettings.js";
 
-let selectedGeo = "Canada"; // "Canada";
+let selectedGeo = "Canada";
 let selectedMonth = "01";
 let selectedYear = "2018";
 let data = {};
@@ -14,6 +14,12 @@ const table = d3.select(".tabledata")
     .attr("id", "modesTable");
 
 function uiHandler(event) {
+  // Clear any text in #zeroFlag
+  if ( d3.select("#zeroFlag").text() !== "") {
+    d3.select("#zeroFlag")
+            .text("");
+  }
+
   if (event.target.id === "groups" || event.target.id === "month" || event.target.id === "year") {
     selectedGeo = document.getElementById("groups").value;
     selectedMonth = document.getElementById("month").value;
@@ -22,24 +28,19 @@ function uiHandler(event) {
     if (!data[selectedYear + "-" + selectedMonth]) {
       d3.json("data/modes/" + selectedYear + "-" + selectedMonth + ".json", function(err, filedata) {
         data[selectedYear + "-" + selectedMonth] = filterZeros(filedata);
-        console.log("new uiHandler data: ", data)
         showData();
       });
     } else {
-      console.log("old uiHandler data: ", data)
-      console.log("old uiHandler selectedGeo: ", selectedGeo)
       const thisData = data[selectedYear + "-" + selectedMonth];
       const thisMonth = i18next.t(selectedMonth, {ns: "modesMonth"});
      
       if (thisData[selectedGeo].links.length === 0) {
-        console.log("zero data[selectedGeo]: ", thisData[selectedGeo])
         d3.selectAll("svg > *").remove();
         d3.select("#zeroFlag")
             .text(`Zero international travellers for ${selectedGeo}, 
               ${thisMonth} ${selectedYear}`);
       }
       else {
-        console.log("no zero links")
         showData();
       }
     }
@@ -62,25 +63,18 @@ function filterZeros(d){
       }
     }
   }
-  console.log("returnObject: ", returnObject)
-  console.log("keys: ", Object.keys(returnObject))
-  const keys = Object.keys(returnObject);
 
-  // Loop through keys and check if all keys.links.length > 0
-  keys.map((d) => {
-    if (returnObject[d].links.length === 0) {
-      console.log("d key: ", d)
-      console.log("returnObject in here: ",returnObject)
-      console.log(returnObject[d].links)
-      console.log(returnObject[d].links.length)
-
-    }
-    
-  });
+  // // Loop through keys and check if all keys.links.length > 0
+  // const keys = Object.keys(returnObject);
+  // keys.map((d) => {
+  //   if (returnObject[d].links.length === 0) {
+  //     console.log("d key: ", d)
+  //     console.log("returnObject in here: ",returnObject)
+  //     console.log(returnObject[d].links)
+  //     console.log(returnObject[d].links.length)
+  //   }
+  // });
   
-
-  // if (returnObject.filter(e => e.name === 'Magenic').length > 0) {
-  // }
   return returnObject;
 }
 
@@ -89,10 +83,7 @@ i18n.load(["src/i18n"], function() {
   d3.queue()
       .defer(d3.json,"data/modes/" + selectedYear + "-" + selectedMonth  + ".json")
       .await(function(error, json) {
-        console.log("load data: ", data)
         data[selectedYear + "-" + selectedMonth] = filterZeros(json);
-        console.log("load data after filterZeros: ", data)
-        console.log("selectedGeo: ", selectedGeo)
         makeSankey(sankeyChart, data[selectedYear + "-" + selectedMonth][selectedGeo]);
       });
 });
