b94d4e9d6ec8004c62427abe2efae304355e6c7c
katiRG <cnangini@gmail.com>
2019-03-22 19:00:28 -0400

    AIR: colourbar spacing box colour changed. Closes Jira 1000
    Signed-off-by: katiRG <cnangini@gmail.com>

--- src/air/index.js
+++ src/air/index.js
@@ -655,7 +655,7 @@ function colorMap() {
   // last 2 colours for blank and NaN box
   const colourArray = [];
   if (selectedDataset === "passengers") {
-    colourArray.push("#AFE2FF", "#72C2FF", "#bc9dff", "#894FFF", "#5D0FBC", "#fff", "#565656");
+    colourArray.push("#AFE2FF", "#72C2FF", "#bc9dff", "#894FFF", "#5D0FBC", "#F9F9F9", "#565656");
   } else {
     colourArray.push("#AFE2FF", "#72C2FF", "#bc9dff", "#894FFF", "#5D0FBC");
   }
