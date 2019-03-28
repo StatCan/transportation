210b6f208b019d515c97afdff3f6f1d9f92ad2e8
katiRG <cnangini@gmail.com>
2019-03-28 10:35:49 -0400

    AIR: hack added for opening table in IE. Closes Jira 924
    Signed-off-by: katiRG <cnangini@gmail.com>

--- src/air/index.js
+++ src/air/index.js
@@ -911,6 +911,48 @@ function getDateMinMax() {
     }
   }
 }
+// ------------------------------------------------------------------------------
+function isIE() {
+  const ua = window.navigator.userAgent;
+  const msie = ua.indexOf("MSIE ");
+  if (msie > 0) {
+    // IE 10 or older => return version number
+    return parseInt(ua.substring(msie + 5, ua.indexOf(".", msie)), 10);
+  }
+  const trident = ua.indexOf("Trident/");
+  if (trident > 0) {
+    // IE 11 => return version number
+    const rv = ua.indexOf("rv:");
+    return parseInt(ua.substring(rv + 3, ua.indexOf(".", rv)), 10);
+  }
+  const edge = ua.indexOf("Edge/");
+  if (edge > 0) {
+    // Edge (IE 12+) => return version number
+    return parseInt(ua.substring(edge + 5, ua.indexOf(".", edge)), 10);
+  }
+  // other browser
+  return false;
+}
+
+function ieWorkAround() {
+  const summaryNode = document.getElementById("chrt-dt-tbl");
+  summaryNode.addEventListener("keydown", function(ev) {
+    if (ev.which == 13 || ev.which == 32) toggle(ev);
+  });
+
+  summaryNode.addEventListener( "click", function(ev) {
+    toggle(ev);
+  });
+
+  function toggle(ev) {
+    ev.preventDefault();
+    ev.stopPropagation();
+    const nodedetails = document.getElementsByClassName("chart-data-table")[0];
+    const isOpen = nodedetails.hasAttribute("open");
+    if (isOpen) nodedetails.removeAttribute("open");
+    else nodedetails.setAttribute("open", "open");
+  }
+}
 // -----------------------------------------------------------------------------
 /* Copy Button*/
 function dataCopyButton(cButtondata) {
@@ -1007,6 +1049,9 @@ i18n.load(["src/i18n"], () => {
 
         // Show chart titles based on default menu options
         updateTitles();
+
+
+        if (isIE()) ieWorkAround();
       });
 });
 
